82371b79252a3e1a242b1e234d728dd1034a2071
Zethuri <anukamvalentine09@gmail.com>
2024-06-11 09:50:08 +0100

    changes made

--- proc-form.php
+++ proc-form.php
@@ -16,18 +16,9 @@
     $conn = mysqli_connect("localhost", "starsandbloom", "valentine", "starsandbloom");
         if (!$conn){
             echo "cannot connect to the server";
-            }
+        }
             
-     $query_chk = "select * from participant where email ='$email' or phone= '$phone' ";
-     $result_chk = mysqli_query($conn,$query_chk); 
-        $num_chk = mysqli_num_rows($result_chk);
-    if ($num_chk > 0)
-    {
-        $error = 'User already exist';
-        include("index.php");
-        exit;
-    }
-    
+
         $query = "insert into participant set fullname = '$name', email = '$email', phone = '$phone', courses='$courses', date= '$date_selected'";
         
         $result = mysqli_query($conn, $query);
